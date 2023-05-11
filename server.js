132efb259d6a6390e75b2d74ebfe8077965d7235
Muhammad Shaheer khan <developer.mshaheerkhan199@gmail.com>
2023-05-12 00:56:15 +0500

    configure sendgrid mailing service for sending mail, configure mail templating engine ejs with partials

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.use(
 )
 app.use(express.json())
 app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
 
 // app.get('/hello')
 
