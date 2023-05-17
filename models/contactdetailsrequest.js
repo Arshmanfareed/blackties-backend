9d06a0d0e49c64300186c27f4e3545d22e6c8496
Muhammad Shaheer khan <developer.mshaheerkhan199@gmail.com>
2023-05-18 00:09:53 +0500

    create table for request contact details

---
+++ models/contactdetailsrequest.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ContactDetailsRequest extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ContactDetailsRequest.init({
+    requesterUserId: DataTypes.INTEGER,
+    requesteeUserId: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    message: DataTypes.INTEGER,
+    status: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ContactDetailsRequest',
+  });
+  return ContactDetailsRequest;
+};
