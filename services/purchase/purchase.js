4c416a6adf430667c54c948efcf7a7e49a7b6b78
Muhammad Shaheer khan <developer.mshaheerkhan199@gmail.com>
2023-05-18 13:37:59 +0500

    create general function for stripe to handle subscription and purchase

--- services/purchase/purchase.js
+++ services/purchase/purchase.js
@@ -1,13 +1,11 @@
 const db = require('../../models')
 const constants = require('../../config/constants')
-const { Op, where, Sequelize } = require('sequelize')
-const { stripe } = require("../../config/stripe")
+const stipeUtils = require('../../utils/stripe')
 
 module.exports = {
   createStripePurchaseLink: async (body, hostAddress, userId) => {
     const { amount } = body
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+    const payload = {
       line_items: [
         {
           price_data: {
@@ -21,22 +19,27 @@ module.exports = {
         },
       ],
       mode: 'payment',
-      success_url: `http://${hostAddress}/dev/mahaba/api/v1/purchase/stripe-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `http://${hostAddress}/dev/mahaba/api/v1/purchase/stripe-cancel`,
       metadata: {
         // Add any other custom data attributes as needed
         userId,
+        type: constants.paymentType.PURCHASE
       },
-    });
+    }
+    const session = await stipeUtils.createCheckoutSession(payload, hostAddress)
     return { sessionUrl: session.url }
   },
   successfullStripePurchase: async (sessionId) => {
-    return stripe.checkout.sessions.retrieve(sessionId)
+    return stipeUtils.retrieveCheckoutSession(sessionId)
       .then(async session => {
         // Handle the successful payment
-        const amountToBeAdded = session.amount_total / 100
-        const { userId } = session.metadata
-        await db.Wallet.increment('amount', { by: amountToBeAdded, where: { userId } })
+        const { userId, type } = session.metadata
+        if (type === constants.paymentType.PURCHASE) { // handle topup payment success
+          const amountToBeAdded = session.amount_total / 100
+          await db.Wallet.increment('amount', { by: amountToBeAdded, where: { userId } })
+        } else { // handle subscription success
+          console.log("session ==> ", session)
+          console.log("Storing  subscription details and changing user plan from free to premium....")
+        }
         // Redirect the user to a success page or display a success message
         return process.env.HOMEPAGE_URL
       })
@@ -47,5 +50,25 @@ module.exports = {
   },
   cancelStripePurchase: async () => {
     return process.env.HOMEPAGE_URL
+  },
+  buyPremiumMembership: async (body, hostAddress, userId) => {
+    const { productId } = body
+    const payload = {
+      line_items: [
+        {
+          price: productId,
+          quantity: 1,
+        },
+      ],
+      mode: "subscription",
+      metadata: {
+        // Add any other custom data attributes as needed
+        userId,
+        productId,
+        type: constants.paymentType.SUBSCRIPTION
+      },
+    }
+    const session = await stipeUtils.createCheckoutSession(payload, hostAddress)
+    return { sessionUrl: session.url }
   },
 }
