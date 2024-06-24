7c932e09a9ba1ce793cd232e69d2c026c6199829
Arshmanfareed <you@example.com>
2024-06-25 03:01:35 +0500

    add empty user API

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -692,6 +692,56 @@ router.post('/:id/request/extra-info', auth, extraInformationRequest, userContro
  */
 router.patch('/request/extra-info/:id/accept-reject', auth, userController.acceptOrRejectExtraInfoRequest)
 
+
+/**
+ * @swagger
+ * /empty-user-data/{id}:
+ *   post:
+ *     summary: Empty the user data
+ *     consumes:
+ *      - application/json
+ *     produces:
+ *      - application/json
+ *     tags:
+ *     - User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               status:
+ *                 type: string
+ *               questions:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: number
+ *                     answer:
+ *                       type: string
+ *     parameters:
+ *     - name: x-auth-token
+ *       in: header
+ *       required: true
+ *       type: string
+ *       description: an authorization header
+ *     - name: id
+ *       in: path
+ *       required: true
+ *       type: string
+ *       description: Id of the extra info request 
+ *     operationId: userDataEmpty
+ *     deprecated: false
+ *     responses:
+ *       '200':
+ *         description: ''
+ *         headers: {}
+ */
+router.patch('/empty-user-data/:id', userController.userDataEmpty)
+
 /**
  * @swagger
  * /user/request/extra-info/question/{id}/cancel:
