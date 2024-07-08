b3fa0c941cc43b6dafec9b49e15956e3537202de
Arshman Fareed <arshmanthakurr555@gmail.com>
2024-07-08 21:11:54 +0500

    integrate new firebase API

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,79 @@
-const FCM = require('fcm-node');
-const serverKey = process.env.FCM_SERVER_KEY;
-const fcm = new FCM(serverKey);
+// const FCM = require('fcm-node');
+// const serverKey = process.env.FCM_SERVER_KEY;
+// const fcm = new FCM(serverKey);
 
-module.exports = fcm
+// module.exports = fcm
+const { google } = require('googleapis');
+const https = require('https');
+const path = require('path');
+
+// const PROJECT_ID = process.env.FCM_PROJECT_ID;
+const PROJECT_ID = 'mahaba-id';
+const HOST = 'fcm.googleapis.com';
+const PATH = `/v1/projects/${PROJECT_ID}/messages:send`;
+const MESSAGING_SCOPE = 'https://www.googleapis.com/auth/firebase.messaging';
+const SCOPES = [MESSAGING_SCOPE];
+
+/**
+ * Get a valid access token.
+ */
+function getAccessToken() {
+  return new Promise((resolve, reject) => {
+    const keyPath = path.join(__dirname, '../placeholders/service-account.json');
+    const key = require(keyPath);
+    const jwtClient = new google.auth.JWT(
+      key.client_email,
+      null,
+      key.private_key,
+      SCOPES,
+      null
+    );
+    jwtClient.authorize((err, tokens) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(tokens.access_token);
+    });
+  });
+}
+
+/**
+ * Send HTTP request to FCM with given message.
+ *
+ * @param {object} fcmMessage The message to send.
+ */
+function sendFcmMessage(fcmMessage) {
+  return getAccessToken().then(accessToken => {
+    const options = {
+      hostname: HOST,
+      path: PATH,
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      }
+    };
+
+    return new Promise((resolve, reject) => {
+      const request = https.request(options, resp => {
+        let data = '';
+        resp.on('data', chunk => {
+          data += chunk;
+        });
+        resp.on('end', () => {
+          resolve(JSON.parse(data));
+        });
+      });
+
+      request.on('error', err => {
+        reject(err);
+      });
+
+      request.write(JSON.stringify(fcmMessage));
+      request.end();
+    });
+  });
+}
+
+module.exports = { sendFcmMessage };
--- utils/push-notification.js
+++ utils/push-notification.js
@@ -1,33 +1,67 @@
-const fcm = require('../config/firebase')
-const notify_tra = require('../config/notification-trans')
+// const fcm = require('../config/firebase')
+// const notify_tra = require('../config/notification-trans')
 
-module.exports = {
-  sendNotificationSingle: async (token, key, body, user, notifyUser) => {
-    if(!token) return false
+// module.exports = {
+//   sendNotificationSingle: async (token, key, body, user, notifyUser) => {
+//     if(!token) return false
 
 
   
-    let title = notify_tra[notifyUser.language][key] ? notify_tra[notifyUser.language][key].title : key;
+//     let title = notify_tra[notifyUser.language][key] ? notify_tra[notifyUser.language][key].title : key;
+//     let description = notify_tra[notifyUser.language][key] ? notify_tra[notifyUser.language][key].message : key;
+
+//     description = description.replace('{{username}}', user.username).replace('{{code}}', user.code);
+
+//     const message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+//       to: token,
+//       collapse_key: title,
+//       notification: { title, body: description },
+//       data: {}, //you can send only notification or only data(or include both)
+//     };
+//     return new Promise((resolve, reject) => {
+//       fcm.send(message, function (err, response) {
+//         if (err) {
+//           console.log('Error sending firebase message:', err, description);
+//           resolve(err)
+//         } else {
+//           console.log("Successfully sent with response: ", response, description);
+//           resolve(response)
+//         }
+//       });
+//     })
+//   },
+// }
+
+const { sendFcmMessage } = require('../config/firebase');
+const notify_tra = require('../config/notification-trans');
+
+module.exports = {
+  sendNotificationSingle: async (token, key, body, user, notifyUser) => {
+    if (!token) return false;
+
+    const title = notify_tra[notifyUser.language][key] ? notify_tra[notifyUser.language][key].title : key;
     let description = notify_tra[notifyUser.language][key] ? notify_tra[notifyUser.language][key].message : key;
 
     description = description.replace('{{username}}', user.username).replace('{{code}}', user.code);
 
-    const message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-      to: token,
-      collapse_key: title,
-      notification: { title, body: description },
-      data: {}, //you can send only notification or only data(or include both)
+    const message = {
+      message: {
+        token,
+        notification: {
+          title,
+          body: description,
+        },
+        data: {}, // Add custom data here if needed
+      }
     };
-    return new Promise((resolve, reject) => {
-      fcm.send(message, function (err, response) {
-        if (err) {
-          console.log('Error sending firebase message:', err, description);
-          resolve(err)
-        } else {
-          console.log("Successfully sent with response: ", response, description);
-          resolve(response)
-        }
-      });
-    })
+
+    try {
+      const response = await sendFcmMessage(message);
+      console.log("Successfully sent with response:", response, description);
+      return response;
+    } catch (err) {
+      console.log('Error sending firebase message:', err, description);
+      return err;
+    }
   },
-}
+};
