e9652c9146af142e702bd2f5e940125553ab3676
Arshmanfareed <131892047+Arshmanfareed@users.noreply.github.com>
2024-07-19 00:11:31 +0500

    Update index.js
    testing changes

--- controllers/index.js
+++ controllers/index.js
@@ -3,4 +3,3 @@ module.exports.profileController = require('../controllers/profile')
 module.exports.userController = require('../controllers/user')
 module.exports.purchaseController = require('../controllers/purchase')
 module.exports.adminController = require('../controllers/admin')
-
